d6c34cac990caf245243f5e02bb204005096966c
justingracie <j.m.gracie@gmail.com>
2022-04-06 12:51:09 -0600

    psuedo code has been added

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-console.log('hello world!')
+// GLOBAL VARIABLES ----->
+
+
+
+// GAME RULES ------>
+
+// --> add event listening to Game Rules button on start screen that links to a 
+//hidden div with game rules written out in <p> tag. 
+
+
+
+
+// START BUTTON ----->
+
+// use addeventlistener to link to page with main game board after user inputs name which 
+//should print to the main game board under player 1. 
+
+
+
+
+// GAMEBOARD ----->
+
+// This is where all the action happens, before starting player two will have a field to 
+//fill out their name and initiate gameplay.  after play two initiates cards will be dealt.
+
+
+// dIVS for  player one and two will have image of cards, flipped cards will display in
+//the battlezone.  will need to an array for deck of cards, then a loop to to iterate 
+//through the array and randomly give every other card to player one and player two. 
+
+//the cards will then be stored in the player constructor objects. 
+//flip card button will drawer from player deck (living in constructor object), and
+//display them in the battle zone. 
+
+//battle zone DIV will need a function to determine highest card.  will need coniditionals
+//that gives the player with the highest all the card within the battlezone, and add them 
+//to their deck/object. 
+//will also need a tie coniditional which requires players to flip again, drawing 3 facedown
+// cards from their decks, and one faceupcard to determine winner of 'war'.  
+
+//and another conditional which initiates another round of war upon a tie, and last conditional which 
+// determines a winner if one players runs out of cards during 'war'.  
+
+//after war will need to call a function to go back into regular gameplay if neither play runs out
+//of cards. 
+
+// END OF GAME ----->
+
+
+//write a function that determines if eithe player has run out of cards, and declare a winner. 
+
+
+
+
+// Play Again ----->
+
+//after the winner is determined I will want a hidden button to unhide with and option to play again
+//this should start us back at the original gameboard with a newly shuffled deck that is dealt out.
+//players names should remain the same. 
+
